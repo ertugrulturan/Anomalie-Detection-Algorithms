2a907f3adc57f0f0e4a53879743a4f1fbb5d9ea8
Ertuğrul TURAN <60829297+ertugrulturan@users.noreply.github.com>
2023-02-01 20:12:29 +0300

    Update ada.lua
    Ratelimit

--- ada.lua
+++ ada.lua
@@ -1,6 +1,8 @@
 local mean = 0
 local std = 0
 local threshold = 3
+local rate_limit = 23
+local window = 1
 
 local function calculate_mean(data)
     local sum = 0
@@ -30,8 +32,39 @@ local function detect_anomaly(data)
     return anomalies
 end
 
+local function rate_limiter(premature, limit, window)
+    if premature then
+        return
+    end
+
+    local count = 0
+    local times = {}
+
+    return function(premature)
+        if premature then
+            return
+        end
+
+        local now = ngx.now()
+        local n = #times
+        while n >= 1 and now - times[n] > window do
+            n = n - 1
+        end
+        table.remove(times, 1, n)
+        count = #times
+        times[#times + 1] = now
+
+        if count >= limit then
+            ngx.log(ngx.ERR, "Rate limit exceeded")
+            return ngx.exit(ngx.HTTP_FORBIDDEN)
+        end
+    end
+end
+
 local data = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 local anomalies = detect_anomaly(data)
 for i = 1, #anomalies do
     ngx.log(ngx.ERR, "Anomaly detected at index: " .. anomalies[i])
 end
+
+ngx.timer.at(0, rate_limiter, rate_limit, window)
